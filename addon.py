a6f30a6539c8b134288e008db00450d876f272ec
Gabriel <gmendrala@gmail.com>
2019-09-11 21:06:25 +0200

    2.0.7
     * Fixed plugin for changes in aktualne web
     * Show pubdate in the infopanel
     * Improved video loading
     * Only active videos showing
     * Clean code

--- addon.py
+++ addon.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 import os
 import sys
 import xbmc
@@ -8,7 +8,6 @@ import xbmcaddon
 import urllib
 import urllib2
 import re
-from stats import *
 import HTMLParser
 import xml.etree.ElementTree as ET
 import email.utils as eut
@@ -18,148 +17,125 @@ import json
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-_rssUrl_ = 'https://video.aktualne.cz/rss/'
+_homepage_ = 'https://video.aktualne.cz/'
+_rssUrl_ = _homepage_+'rss/'
 
 _addon_ = xbmcaddon.Addon('plugin.video.dmd-czech.aktualne')
 _lang_   = _addon_.getLocalizedString
 _scriptname_ = _addon_.getAddonInfo('name')
-_baseurl_ = 'https://video.aktualne.cz/'
-_homepage_ = 'https://video.aktualne.cz/forcedevice/smart/'
-_UserAgent_ = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
+_UserAgent_ = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, lkodiazor Gecko) Chrome/52.0.2743.116 Safari/537.36'
 _quality_ = _addon_.getSetting('quality')
-_firetvhack_ = _addon_.getSetting('firetvhack') == "true"
 home = _addon_.getAddonInfo('path')
-_icon_ = xbmc.translatePath( os.path.join(home, 'resources/media/ikona-aktualne-57x57.png' ) )
-_mediadir_ = xbmc.translatePath( os.path.join(home, 'resources/media/' ) )
+_icon_ = xbmc.translatePath(os.path.join(home, 'resources/media/ikona-aktualne-57x57.png'))
+_mediadir_ = xbmc.translatePath(os.path.join(home, 'resources/media/'))
 _htmlParser_ = HTMLParser.HTMLParser()
-_dialogTitle_ = 'Aktuálně TV'
-
-fanart = xbmc.translatePath( os.path.join( home, 'fanart.jpg' ) )
-
-if _quality_ == '':
-    xbmc.executebuiltin("XBMC.Notification('Doplněk Aktuálně','Vyberte preferovanou kvalitu!',30000,"+_icon_+")")
-    _addon_.openSettings() 
 
 def log(msg, level=xbmc.LOGDEBUG):
-	if type(msg).__name__=='unicode':
-		msg = msg.encode('utf-8')
-        xbmc.log("[%s] %s"%(_scriptname_,msg.__str__()), level)
+    if type(msg).__name__ == 'unicode':
+        msg = msg.encode('utf-8')
+    xbmc.log("[%s] %s" % (_scriptname_, msg.__str__()), level)
 
 def logDbg(msg):
-	log(msg,level=xbmc.LOGDEBUG)
+    log(msg,level=xbmc.LOGDEBUG)
 
 def logErr(msg):
-	log(msg,level=xbmc.LOGERROR)
+    log(msg,level=xbmc.LOGERROR)
 
 addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'episodes')
 
 def showNotification(message, icon):
-	xbmcgui.Dialog().notification(_dialogTitle_, message, icon)
+    xbmcgui.Dialog().notification(_scriptname_, message, icon)
 
 def showErrorNotification(message):
-	showNotification(message, 'error')
+    showNotification(message, 'error')
 
 def fetchUrl(url, label):
-	logDbg("fetchUrl " + url + ", label:" + label)
-	httpdata = ''	
-	try:
-		resp = urllib2.urlopen(url)
-		size = resp.info().getheader('Content-Length', 9000)
-		for line in resp:
-			httpdata += line
-	except:
-		httpdata = None
-		showErrorNotification(_lang_(30002))
-	finally:
-		resp.close()	
-	return httpdata
+    logDbg("fetchUrl " + url + ", label:" + label)
+    httpdata = ''   
+    try:
+        request = urllib2.Request(url, headers={'User-Agent': _UserAgent_,})
+        resp = urllib2.urlopen(request)
+        httpdata = resp.read()
+    except:
+        httpdata = None
+        showErrorNotification(_lang_(30002))
+    finally:
+        resp.close() 
+    return httpdata
 
 def listItems(offset, urladd):
-	url = _rssUrl_ + urladd
-	if offset > 0:
-		url += '/?offset=' + str(offset)
-	rss = fetchUrl(url, _lang_(30003))
-	if (not rss):
-		return
-	root = ET.fromstring(rss)
-	for item in root.find('channel').findall('item'):
-		link  = item.find('link').text
-		title = item.find('title').text
-		description = item.find('description').text
-		contentEncoded = item.find('{http://purl.org/rss/1.0/modules/content/}encoded').text
-		extra = item.find('{http://i0.cz/bbx/rss/}extra')
-		subtype = extra.get('subtype')
-		dur = extra.get('duration')
-		datetime = eut.parsedate(item.find('pubDate').text.strip())
-		date = time.strftime('%d.%m.%Y', datetime)
-		image = re.compile('<img.+?src="([^"]*?)"').search(contentEncoded).group(1)
-		li = xbmcgui.ListItem(title)
-		if dur and ':' in dur:
-			l = dur.strip().split(':')
-			duration = 0
-			for pos, value in enumerate(l[::-1]):
-				duration += int(value) * 60 ** pos
-			li.addStreamInfo('video', {'duration': duration})
-		if subtype == 'playlist':
-			li.setLabel2('Playlist')
-		li.setThumbnailImage(image)
-		li.setIconImage(_icon_)
-		li.setInfo('video', {'title': title, 'plot': description, 'date': date})
-		li.setProperty('fanart_image',image)
-		u=sys.argv[0]+'?mode=10&url='+urllib.quote_plus(link.encode('utf-8'))
-		xbmcplugin.addDirectoryItem(handle=addon_handle, url=u, listitem=li)
-	o = offset + 30	
-	u = sys.argv[0]+'?mode=1&url='+urllib.quote_plus(urladd.encode('utf-8'))+'&offset='+urllib.quote_plus(str(o))
-	liNext = xbmcgui.ListItem(_lang_(30006))
-	xbmcplugin.addDirectoryItem(handle=addon_handle,url=u,listitem=liNext,isFolder=True)	
-	xbmcplugin.endOfDirectory(addon_handle)
+    url = _rssUrl_ + urladd
+    if offset > 0:
+        url += '?offset=' + str(offset)
+    rss = fetchUrl(url, _lang_(30003))
+    if (not rss):
+        return
+    root = ET.fromstring(rss)
+    for item in root.find('channel').findall('item'):
+        link  = item.find('link').text
+        title = item.find('title').text
+        description = item.find('description').text
+        contentEncoded = item.find('{http://purl.org/rss/1.0/modules/content/}encoded').text
+        extra = item.find('{http://i0.cz/bbx/rss/}extra')
+        subtype = extra.get('subtype')
+        dur = extra.get('duration')
+        datetime = eut.parsedate(item.find('pubDate').text.strip())
+        date = time.strftime('%d.%m.%Y', datetime)
+        image = re.compile('<img.+?src="([^"]*?)"').search(contentEncoded).group(1)
+        li = xbmcgui.ListItem(title)
+        if dur and ':' in dur:
+            l = dur.strip().split(':')
+            duration = 0
+            for pos, value in enumerate(l[::-1]):
+                duration += int(value) * 60 ** pos
+            li.addStreamInfo('video', {'duration': duration})
+        if subtype == 'playlist':
+            li.setLabel2('Playlist')
+        li.setThumbnailImage(image)
+        li.setIconImage(_icon_)
+        li.setInfo('video', {'mediatype': 'episode', 'title': title, 'plot': description, 'premiered': date})       
+       
+        li.setProperty('fanart_image',image)
+        u=sys.argv[0]+'?mode=10&url='+urllib.quote_plus(link.encode('utf-8'))
+        if(dur):
+            xbmcplugin.addDirectoryItem(handle=addon_handle, url=u, listitem=li)
+    o = offset + 30 
+    u = sys.argv[0]+'?mode=1&url='+urllib.quote_plus(urladd.encode('utf-8'))+'/&offset='+urllib.quote_plus(str(o))
+    liNext = xbmcgui.ListItem(_lang_(30006))
+    xbmcplugin.addDirectoryItem(handle=addon_handle,url=u,listitem=liNext,isFolder=True)    
+    xbmcplugin.endOfDirectory(addon_handle)
 
 def playUrl(url):
-	httpdata = fetchUrl(url, _lang_(30004))
-	twice = False
-	if (not httpdata):
-		return
-	if httpdata: 
-		title = re.compile('<meta property="og:title" content=".*">').search(httpdata).group(0)
-		title = re.sub('<meta property="og:title" content="', '', title).replace('">', '')
-		image = re.compile('<meta property="og:image" content=".*">').search(httpdata).group(0)
-		image = re.sub('<meta property="og:image" content="', '', image).replace('">', '')
-		description = re.compile('<meta property="og:description" content=".*">').search(httpdata).group(0)
-		description = re.sub('<meta property="og:description" content="', '', description).replace('">', '')
-		videos = re.compile('tracks:(?:.(?!\}\]\}))*.\}\]\}', re.S).findall(httpdata)
-		if len(videos) > 1:  # last item in playlist is doubled on page
-			del videos[-1]
-		if videos:
-			pl=xbmc.PlayList(1)
-			pl.clear()
-			if _firetvhack_ and len(videos) == 1:
-				twice = True
-			for video in videos:
-				video = re.sub(re.compile('\sadverttime:.*', re.S), '', video) # live streams workaround
-				video = video.replace('tracks: ', '')
-				video = re.sub(r'[,\w]*$','',video)
-				try:
-					detail = json.loads(video)
-				except ValueError:
-					showErrorNotification(_lang_(30005))
-					return
-				if detail.has_key('MP4'):
-					sources = detail['MP4']
-					for version in sources:
-						url = version['src']
-						quality = version['label']
-						li = xbmcgui.ListItem(title)
-						li.setThumbnailImage(image)
-						li.addStreamInfo('video', {'language': 'cs'})
-						if (quality == _quality_):
-							xbmc.PlayList(1).add(url, li)
-							if twice:
-								xbmc.PlayList(1).add(url, li)
-			xbmc.Player().play(pl)
-		else:
-			showErrorNotification(_lang_(30005))
+    httpdata = fetchUrl(url, _lang_(30004))
+    if (not httpdata):
+        return
+    if httpdata: 
+        title = re.search('<meta property="og:title" content="(.+)">', httpdata).group(1)
+        image = re.search('<meta property="og:image" content="(.+)">', httpdata).group(1)
+        videos = re.search('"MP4":(.+\])', httpdata).group(1)
+
+    try:
+        detail = json.loads(videos)
+    except ValueError:
+        showErrorNotification(_lang_(30005))
+        return
+    if detail:
+        pl=xbmc.PlayList(1)
+        pl.clear()
+
+        for version in detail:
+            url = version['src']
+            quality = version['label']
+            li = xbmcgui.ListItem(title)
+            li.setThumbnailImage(image)
+            li.addStreamInfo('video', {'language': 'cs'})
+            if (quality == _quality_):
+                xbmc.PlayList(1).add(url, li)
+                xbmc.Player().play(pl)
+    else:
+        showErrorNotification(_lang_(30005))
 
 def get_params():
         param=[]
@@ -179,37 +155,24 @@ def get_params():
         return param
 
 def listShows():
-    request = urllib2.Request(_homepage_)
+    hdr = {'User-Agent': _UserAgent_,}
+    request = urllib2.Request(_homepage_, headers=hdr)
     con = urllib2.urlopen(request)
     data = con.read()
     con.close()
-    match = re.compile('<a href="/#porady">.+?</a>.+?<ul class="dropdown">(.+?)</ul>', re.DOTALL).findall(data)
-    if len(match):
-      match2 = re.compile('<li class="menusec-bvs.+?"><a href="/(.+?)/">(.+?)</a></li>').findall(match[0])
-      if len(match2) > 1:
-          for url2, name in match2:
-              addDir(name,url2,1)  
-    else:
-      #add all without parsing, just in case of the change of the page layout
-      addDir(u'DV TV','dvtv',1)
-      addDir(u'Petr Čtvrtníček TV','petr-ctvrtnicek-tv',1)
+
+    match = re.compile('<h2 class="section-title"><a href="/(.+?)/">(.+?)</a></h2>', re.DOTALL).findall(data)
+    if len(match) > 1:
+        for url, name in match:
+            addDir(name,url,1)  
     xbmcplugin.endOfDirectory(addon_handle)
     
 def addDir(name,url,mode):
-    iconimage = _mediadir_ + url + ".jpg"
-    logDbg("addDir(): '"+name+"' url='"+url+"' icon='"+iconimage+"' mode='"+str(mode)+"'")
+    logDbg("addDir(): '"+name+"' url='"+url+"' icon='' mode='"+str(mode)+"'")
     u=sys.argv[0]+"?url="+urllib.quote_plus(url.encode('utf-8'))+"&mode="+str(mode)+"&name="+urllib.quote_plus(name.encode('utf-8'))
     ok=True
-    liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)   
+    liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage='')   
     liz.setInfo( type="Video", infoLabels={ "Title": name } )
-    changer = {
-        "zkrotte-sve-penize": "zkrotte-sve-penize.png",
-        "ze-sveta": "zesveta.jpg",
-        "48-hodin-v": "48-hodin.jpg",
-        "ego-night": "egonight.jpg",
-    }
-    urlfanart = changer.get(url, url + ".jpg")
-    liz.setProperty( "Fanart_Image", "http://i0.cz/bbx/video/img/video/porad-d-page-" + urlfanart )
     ok=xbmcplugin.addDirectoryItem(handle=addon_handle,url=u,listitem=liz,isFolder=True)
     return ok
 
@@ -238,13 +201,12 @@ try:
 except:
         pass
 
+
+
 if mode==None or url==None or len(url)<1:
-    STATS("OBSAH", "Function")
     listShows()
     logDbg("List Shows end")
 elif mode==1:
-    STATS("listItems", "Function")
     listItems(offset, url)
 elif mode==10:
-    STATS(url, "Item")
     playUrl(url)
