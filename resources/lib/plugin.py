7378c1b0a3a8db38d6dc1d2afe7ee52e7cd22d43
Gabriel <20426629+wombat01@users.noreply.github.com>
2020-08-22 13:11:19 +0200

    [plugin.video.aktualne.cz] 2.2.2

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -31,34 +31,38 @@ def list_shows():
         title = porad.text.encode('utf-8')            
         list_item = xbmcgui.ListItem(title)
         list_item.setInfo('video', {'mediatype': 'tvshow', 'title': title})
-        listing.append((plugin.url_for(get_list, porad['href'], 2, 0), list_item, True))
+        listing.append((plugin.url_for(get_list, show_id = porad['href'], category = 2, page = 0), list_item, True))
         
     xbmcplugin.addDirectoryItems(plugin.handle, listing, len(listing))
     xbmcplugin.endOfDirectory(plugin.handle)
     
-@plugin.route('/get_list/<path:show_id>/<category>/<page>')
-def get_list(show_id, category, page):
+@plugin.route('/get_list/')
+def get_list():
     xbmcplugin.setContent(plugin.handle, 'episodes')
+    show_id = plugin.args['show_id'][0] if 'show_id' in plugin.args else ''
+    page = int(plugin.args['page'][0] if 'page' in plugin.args else 0)
+    print page
+    category = int(plugin.args['category'][0] if 'category' in plugin.args else 0)
     url = _baseurl+'rss{0}?offset={1}'.format(show_id, page)
     listing = []
     count = 0
     root = ET.fromstring(get_page(url))
     for item in root.find('channel').findall('item'):
-        duration = 0
         menuitems = []
         title = item.find('title').text.encode('utf-8')
         title_label = title
         show_title = re.compile('(.+?) -').search(root.find('.//channel/title').text).group(1)
-        if category == '1':
+        if category == 1:
             show_title = item.find('category').text.encode('utf-8')
             title_label = '[COLOR blue]{0}[/COLOR] · {1}'.format(show_title, title)
             show_id = re.compile('\/\/.+?(\/.+?)\/').search(item.find('link').text).group(1)
-            menuitems.append(( _addon.getLocalizedString(30004), 'XBMC.Container.Update('+plugin.url_for(get_list, show_id, 0, 0)+')' )) #get_list, '', 'recent', 0)
+            menuitems.append(( _addon.getLocalizedString(30004), 'XBMC.Container.Update('+plugin.url_for(get_list, show_id = show_id, category = 0, page = 0)+')' )) #get_list, '', 'recent', 0)
         thumb = re.compile('<img.+?src="([^"]*?)"').search(item.find('{http://purl.org/rss/1.0/modules/content/}encoded').text).group(1)
         desc = item.find('description').text
         date = datetime.datetime(*(time.strptime(item.find('pubDate').text.strip()[:16], '%a, %d %b %Y')[:6])).strftime("%Y-%m-%d")
         dur = item.find('{http://i0.cz/bbx/rss/}extra').get('duration')
         if dur and ':' in dur:
+            duration = 0
             l = dur.strip().split(':')
             for pos, value in enumerate(l[::-1]):
                 duration += int(value) * 60 ** pos
@@ -69,18 +73,18 @@ def get_list(show_id, category, page):
         list_item.addContextMenuItems(menuitems)
         listing.append((plugin.url_for(get_video, item.find('link').text), list_item, False))
         count +=1
-
-    if count>=30:
+    print page
+    if count>=30 and category != 1:
         list_item = xbmcgui.ListItem(label=_addon.getLocalizedString(30003))
         list_item.setArt({'icon': 'DefaultFolder.png'})
-        listing.append((plugin.url_for(get_list, show_id, category, int(page) + 30), list_item, True))
+        listing.append((plugin.url_for(get_list, show_id = show_id, category = category, page = page + 30), list_item, True))
             
     xbmcplugin.addDirectoryItems(plugin.handle, listing, len(listing))
     xbmcplugin.endOfDirectory(plugin.handle)
     
-@plugin.route('/get_video/<path:slug_url>')
-def get_video(slug_url):
-    soup = BeautifulSoup(get_page(slug_url), 'html.parser')
+@plugin.route('/get_video/<path:show_url>')
+def get_video(show_url):
+    soup = BeautifulSoup(get_page(show_url), 'html.parser')
     data = json.loads(re.compile('\"tracks\":(.+?),\"adverttime').findall(soup.get_text())[0])
     if(data['HLS']):
         stream_url = data['HLS'][0]['src']
@@ -95,7 +99,7 @@ def root():
     listing = []
     list_item = xbmcgui.ListItem(_addon.getLocalizedString(30001))
     list_item.setArt({'icon': 'DefaultRecentlyAddedEpisodes.png'})
-    listing.append((plugin.url_for(get_list, '', 1, 0), list_item, True))
+    listing.append((plugin.url_for(get_list, show_id = '', category = 1, page = 0), list_item, True))
     
     list_item = xbmcgui.ListItem(_addon.getLocalizedString(30002))
     list_item.setArt({'icon': 'DefaultTVShows.png'})
